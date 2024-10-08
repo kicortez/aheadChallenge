f70e9a7c1fe22abd17834dc93034bcaf214b147c
Kim Cortez <kimislecortez@gmail.com>
2024-10-08 19:12:51 +0800

    Initial Commit

---
+++ aheadChallenge/aheadChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aheadChallengeApp.swift
+//  aheadChallenge
+//
+//  Created by Kim Cortez on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct aheadChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
